153c7d00e8474cb4744b81541fcce3a49c6a06ea
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2018-03-12 10:05:15 +0700

    init master

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h2>
+          Did you see what I saw?
+        </h2>
+        <p>
+          {process.env.DIDYOUSEE}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
